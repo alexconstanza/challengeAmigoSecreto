1ba74f47a624e2dcfd95ef2be7dec524d9cf4850
Alexander Constanza <alexconstanza@gmail.com>
2025-02-12 14:47:04 -0600

    Primera Actualizacion

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+function agregarAmigo(){
+    let amigos = [];
+    let temp = document.querySelector("input");
+    amigos.push(temp);
+    //console.log(amigos);
+    console.log(temp)
+}
