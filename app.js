da88f7faac613f04610bb61e6e1789cf5ce935a6
Alexander Constanza <alexconstanza@gmail.com>
2025-03-12 21:46:13 -0600

    Solo falta mostrar

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-
+//let amigo = document.querySelector('h3');
+let conjuntoAmigos = [];
 function agregarAmigo(){
-    let amigos = [];
-    let temp = document.querySelector("input");
-    amigos.push(temp);
-    //console.log(amigos);
-    console.log(temp)
+    let amigosIngreso = document.getElementById('amigo').value;
+    if(amigosIngreso === ""){
+        alert("Ingrese el nombre de un participante.")
+    }   else {
+        conjuntoAmigos.push(amigosIngreso);
+        console.log(conjuntoAmigos);
+    }
+    limpiarCaja();
 }
+
+function limpiarCaja(){
+    document.querySelector('#amigo').value ='';
+}
+function sortearAmigo(){
+    let tamano = conjuntoAmigos.length;
+    let numeroSorteo = Math.floor(Math.random()*tamano)+1;
+    let verificarRifa = numeroSorteo-1;
+    console.log(numeroSorteo);
+    console.log(conjuntoAmigos[verificarRifa]);
+    console.log(verificarRifa);
+}
+    
+
