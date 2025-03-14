5bce2270a4705c293d337bd48a5a609bae37c551
Alexander Constanza <alexconstanza@gmail.com>
2025-03-14 15:17:17 -0600

    viernes 14mar025

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 //let amigo = document.querySelector('h3');
 let conjuntoAmigos = [];
+
+
+
+
+
 function agregarAmigo(){
     let amigosIngreso = document.getElementById('amigo').value;
     if(amigosIngreso === ""){
@@ -22,6 +27,27 @@ function sortearAmigo(){
     console.log(numeroSorteo);
     console.log(conjuntoAmigos[verificarRifa]);
     console.log(verificarRifa);
+    return conjuntoAmigos[verificarRifa];
 }
-    
+let guardarValor = sortearAmigo();
+
+const parrafoCuatro = document.createElement('p');
+const textoParrafoCuatro = document.createTextNode('Párrafo 4');
+const elementoPadre = document.querySelector ('.padre');
+elementoPadre.appendChild(parrafoCuatro);
+parrafoCuatro.appendChild(textoParrafoCuatro);
+/*const itemAgregado = document.createElement('li');
+const textItemAgregado = document.createTextNode("Parrafo 4");
+const amigoGanador = document.querySelector('.result-list');
+amigoGanador.appendChild(itemAgregado);
+*/
+/*function mostrarAmigo(){
+    let amigoSeleccionado = document.querySelector("result-list");
+    let li1 = document.createElement("li");
+    li1.textContent = conjuntoAmigos
+
+let itemAgregado = document.createElement("li");
+itemAgregado.textContent = guardarValor;
+amigoGanador.appendChild(itemAgregado);
+ }*/   
 
